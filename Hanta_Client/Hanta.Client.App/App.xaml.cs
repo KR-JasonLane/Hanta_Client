f600bd049e867369a14ec63d14b3250d6d5a7c95
KR-JasonLane <malbox5034@naver.com>
2024-05-13 21:21:34 +0900

    프로젝트 최초 생성
     * Git ignore 수정
     * MVVM 패턴 뼈대 구축

---
+++ Hanta_Client/Hanta.Client.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Hanta.Client.App;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+	private void OnStartUp(object sender, StartupEventArgs e)
+	{
+
+    }
+}
---
+++ Hanta_Client/Hanta.Client.Views/Main/MainWindow.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace Hanta.Client.App;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window { public MainWindow() => InitializeComponent(); }
